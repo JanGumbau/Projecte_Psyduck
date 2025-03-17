8fd4b39027d37f4a0574ab9915807550749dd99e
Nil Bosch <nil.bosch@estudiant.enti.cat>
2025-03-17 13:49:48 +0100

    Menu inicial

---
+++ Assets/Scripts/canvi_escenea.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class canvi_escenea : MonoBehaviour
+{
+    
+    public void OnButtonClick()
+    {
+        SceneManager.LoadScene("SampleScene");
+
+        
+    }
+
+  
+}
