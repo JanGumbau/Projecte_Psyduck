ce2adf745ce97f4a81ed8a26dc0acdfd8329202b
Jan Gumbau <jan.gumbau@enti.cat>
2025-04-16 19:45:43 +0200

    Revert "Merge branch 'feature/URPToBRP' into develop"
    This reverts commit 6413944f7f3db2e94e440bda9df443f8b85b284c, reversing
    changes made to e86a037d2b9fc71093f7320eca449755bbda9de4.

---
+++ Assets/Scripts/canvi_escenea.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class canvi_escenea : MonoBehaviour
+{
+
+    public void LoadScene(string nextScene)
+    {
+        SceneManager.LoadScene("level1_1");
+
+        
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }  
+}
