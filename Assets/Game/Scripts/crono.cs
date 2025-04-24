6653b75b28160d6ffcd83d15264c42ec55f0cbd1
Nilisim <nil.bosch@enti.cat>
2025-04-24 19:31:11 +0200

    Cronometre Arreglat

--- Assets/Game/Scripts/crono.cs
+++ Assets/Game/Scripts/crono.cs
@@ -5,50 +5,38 @@ using TMPro;
 
 public class crono : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI textoCrono;
+    private TextMeshProUGUI textoCrono;
     [SerializeField] private float tiempo = 0;
-    
-
-    
 
     private int tiempoMinuto = 0;
     private int tiempoDecimasSegundo = 0;
     private int tiemposegundos = 0;
 
+    void Start()
+    {
+        textoCrono = GameObject.Find("TextoCronoUI").GetComponent<TextMeshProUGUI>();
+        if (textoCrono == null)
+        {
+            Debug.LogError("No se encontró el objeto TextoCronoUI con TextMeshProUGUI");
+        }
+    }
 
     void Cronometro()
     {
-       
-        
-            tiempo += Time.deltaTime;
-     
+        tiempo += Time.deltaTime;
+
         tiempoMinuto = Mathf.FloorToInt(tiempo / 60);
         tiemposegundos = Mathf.FloorToInt(tiempo % 60);
-        tiempoDecimasSegundo = Mathf.FloorToInt((tiempo%1)*100);
+        tiempoDecimasSegundo = Mathf.FloorToInt((tiempo % 1) * 100);
 
         textoCrono.text = string.Format("{0:00}:{1:00}:{2:00}", tiempoMinuto, tiemposegundos, tiempoDecimasSegundo);
-        if(tiempo<=5)
-        {
-            
-           
-           
-        }
-        if(tiempo>=7)
-        {
-            
-            
-        }
-        if(tiempo>=10)
-        {
-         
-           
-        }
     }
-    
 
- 
     void Update()
     {
-        Cronometro(); 
+        if (textoCrono != null)
+        {
+            Cronometro();
+        }
     }
 }
