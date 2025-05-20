bc45ca9933d621d5bd0c15c777cce0f7d2faec00
Nil Bosch <nil.bosch@estudiant.enti.cat>
2025-05-20 11:54:26 +0200

    enemic canvia de color quan fas hit

--- Assets/Game/Scripts/Enemy.cs
+++ Assets/Game/Scripts/Enemy.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private EnemyManager enemyManager;
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
     public static int enemiesDestroyed; // Track enemies destroyed
 
 
     void Awake()
     {
         EnemyManager.Instance.enemiesCount++; // Increment enemies count when an enemy is created
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
     }
-
+    public void RebreDany()
+    {
+        StartCoroutine(CanviColorTemporal(Color.red, 0.2f)); // Canvi a vermell durant 1 segon
+    }
+    private System.Collections.IEnumerator CanviColorTemporal(Color nouColor, float duracio)
+    {
+        spriteRenderer.color = nouColor;
+        yield return new WaitForSeconds(duracio);
+        spriteRenderer.color = originalColor;
+    }
 
 
     public void OnCollisionEnter2D(Collision2D collision)
